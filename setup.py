d809cc0bb2600d59e9bf7a60149de469a73affce
Jan Bremec <160834956+JanBremec@users.noreply.github.com>
2025-09-24 12:24:51 +0200

    Bump version from 0.3 to 0.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ sharedfile_module = Extension(
 
 setup(
     name='sharedfile',
-    version='0.3',
+    version='0.6',
     description='Cross-platform shared file open with share flags',
     long_description=open("README.md").read(),
     long_description_content_type='text/markdown',
